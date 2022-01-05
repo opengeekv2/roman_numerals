9aedef964a4db206b2903ad7b77b7edcc5f9c053
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-01-05 10:59:39 +0100

    Refactor test case where number value is 2

--- src/test/kotlin/com/codurance/craftingcode/romanumerals/RomanNumeralsTest.kt
+++ src/test/kotlin/com/codurance/craftingcode/romanumerals/RomanNumeralsTest.kt
@@ -10,7 +10,6 @@ class RomanNumeralsTest {
     @ParameterizedTest
     @CsvSource(
         "I, 1",
-        "II, 2",
         "V, 5",
         "X, 10",
         "L, 50",
@@ -18,9 +17,21 @@ class RomanNumeralsTest {
         "D, 500",
         "M, 1000"
     )
-    fun testNumbers(roman: String, number: Int) {
+    fun testSingleLetterNumbers(roman: String, number: Int) {
+        assertsNumbers(roman, number)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "II, 2",
+    )
+    fun testWhenNumber2ThenII(roman: String, number: Int) {
+        assertsNumbers(roman, number)
+    }
+
+    private fun assertsNumbers(roman: String, number: Int) {
         val result = convert(number)
-        assertEquals(roman, result)
+        assertEquals(roman, result, "")
     }
 }
 
