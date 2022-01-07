f074cf638f7726cf54e2116f7d0a8c8da691a46c
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-01-07 17:20:36 +0100

    Refactor: Generalize ifs into a while

--- src/main/kotlin/com/codurance/craftingcode/romanumerals/RomanNumerals.kt
+++ src/main/kotlin/com/codurance/craftingcode/romanumerals/RomanNumerals.kt
@@ -10,28 +10,10 @@ fun convert(number: Int): String {
     romanDigits[4] = "IV"
     romanDigits[1] = "I"
 
-    if (number >= 40) {
-        return romanDigits[40]!! + convert(number - 40)
-    }
-
-    if (number >= 10) {
-        return romanDigits[10]!! + convert(number - 10)
-    }
-
-    if (number >= 9) {
-        return romanDigits[9]!! + convert(number - 9)
-    }
-
-    if (number >= 5) {
-        return romanDigits[5]!! + convert(number - 5)
-    }
-
-    if (number >= 4) {
-        return romanDigits[4]!! + convert(number - 4)
-    }
-
-    if (number >= 1) {
-        return romanDigits[1]!! + convert(number - 1)
+    for ((value, romanDigit) in romanDigits) {
+        if (number >= value) {
+            return romanDigit + convert(number - value)
+        }
     }
 
     return ""
