0f9e330903c4e791183059fb661e0ed37c626fea
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-01-07 17:19:34 +0100

    Refactor: Give all Hash Map elements an if so they are all equal

--- src/main/kotlin/com/codurance/craftingcode/romanumerals/RomanNumerals.kt
+++ src/main/kotlin/com/codurance/craftingcode/romanumerals/RomanNumerals.kt
@@ -2,7 +2,7 @@ package com.codurance.craftingcode.romanumerals
 
 fun convert(number: Int): String {
 
-    val romanDigits = HashMap<Int, String>()
+    val romanDigits = LinkedHashMap<Int, String>()
     romanDigits[40] = "XL"
     romanDigits[10] = "X"
     romanDigits[9] = "IX"
@@ -10,20 +10,28 @@ fun convert(number: Int): String {
     romanDigits[4] = "IV"
     romanDigits[1] = "I"
 
-    if (romanDigits.containsKey(number)) {
-        return romanDigits[number]!!
+    if (number >= 40) {
+        return romanDigits[40]!! + convert(number - 40)
     }
 
-    if (number > 40) {
-        return convert(40) + convert(number - 40)
+    if (number >= 10) {
+        return romanDigits[10]!! + convert(number - 10)
     }
 
-    if (number > 10) {
-        return convert(10) + convert(number - 10)
+    if (number >= 9) {
+        return romanDigits[9]!! + convert(number - 9)
     }
 
-    if (number > 0) {
-        return convert(number-1) + convert(1)
+    if (number >= 5) {
+        return romanDigits[5]!! + convert(number - 5)
+    }
+
+    if (number >= 4) {
+        return romanDigits[4]!! + convert(number - 4)
+    }
+
+    if (number >= 1) {
+        return romanDigits[1]!! + convert(number - 1)
     }
 
     return ""
